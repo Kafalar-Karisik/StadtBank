ffc5bd430aa08ed798bb3b964f4b7e22b24424b8
Kafalar-Karisik <orsmes6145@gmail.com>
2023-12-15 16:39:50 +0100

    Template Updates

--- Actions/static/scripts.js
+++ Actions/static/scripts.js
@@ -1,12 +1,12 @@
 var refreshIcon = document.getElementById("refresh");
 
-refreshIcon.addEventListener("click", function () {
-  refreshIcon.classList.add("rotate-animation");
+if (refreshIcon) {
+  refreshIcon.addEventListener("click", function () {
+    refreshIcon.classList.add("rotate-animation");
+    location.reload();
+  });
+}
 
-  // Yenileme butonuna tıklandığında yapılacak işlemler
-  // Örnek olarak sayfayı yenilemek için:
-  location.reload();
-});
 
 
 function customerDetails(nr) {
@@ -16,3 +16,33 @@ function customerDetails(nr) {
 function actionDetails(nr) {
   window.location.href = window.location.origin + "/actions/" + nr
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll('.tabs li');
+  const tabContents = document.querySelectorAll('.tab-content');
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      // Remove is-active class from all tabs and is-hidden from all tab contents
+      tabs.forEach((t) => t.classList.remove('is-active'));
+      tabContents.forEach((tc) => tc.classList.add('is-hidden'));
+
+      // Add is-active class to the clicked tab and display the corresponding content
+      tab.classList.add('is-active');
+      tabContents[index].classList.remove('is-hidden');
+    });
+  });
+});
+
+
+/*
+const inputElements = document.querySelectorAll('.input');
+
+function toggleInputStates() {
+  inputElements.forEach(input => {
+    input.classList.toggle('is-active');
+    input.classList.toggle('is-loading');
+  });
+}
+*/
--- Actions/urls.py
+++ Actions/urls.py
@@ -5,8 +5,10 @@ from . import views
 
 urlpatterns = [
     path('customers/', views.Customers.as_view(), name='kunden_list'),
-    path("customers/<int:nr>/", views.CustomerDV.as_view(), name="detail"),
+    path("customers/<int:nr>/", views.CustomerDV.as_view(), name="customer_detail"),
     path('actions/', views.Actions.as_view(), name='action_list'),
+    path("actions/<int:id>/", views.ActionDV.as_view(), name="action_detail"),
     path('pay-in/',views.pay_in),
     path('pay-out/', views.pay_out),
+    path('settings/', views.Settings.as_view())
 ]
