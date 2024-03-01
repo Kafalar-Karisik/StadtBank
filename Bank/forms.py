1bbb0ffcb746a4b447cbab1cabd8202d22f730af
Kafalar-Karisik <orsmes6145@gmail.com>
2024-03-01 16:54:38 +0100

    Form Upload

--- Bank/forms.py
+++ Bank/forms.py
@@ -6,3 +6,11 @@ class CustomerF(forms.Form):
     """Customer From"""
     nr = forms.IntegerField(label="Customer Nr")
     amount = forms.FloatField()
+
+
+class TransferF(forms.Form):
+    """Transfer Form"""
+    nr = forms.ImageField()
+    type = "transfer"
+    amount = forms.FloatField()
+    releated_nr = forms.IntegerField()
