9fda0557529510b7fa590dd9dc438dee0cfbfb3e
Kafalar-Karisik <orsmes6145@gmail.com>
2024-04-12 15:51:01 +0200

    Script Update

--- bin/userSetup.py
+++ bin/userSetup.py
@@ -2,17 +2,16 @@ import os
 import sys
 
 import django
-
-sys.path.append(os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..'))) 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'StadtBank.settings')
-django.setup()
-
 from django.contrib.auth.models import Group, Permission, User
 from django.contrib.contenttypes.models import ContentType
 
 from bin.TOTP import newWorkerPassword
+
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..')))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'StadtBank.settings')
+django.setup()
 
 
 # User.objects.create_superuser(
@@ -22,5 +21,5 @@ User.objects.create_user("worker", "", "").save()
 print(newWorkerPassword())
 Group_L4 = Group.objects.create(name="Authorized-L4")
 Group_L4.permissions.add(
-    *Permission.objects.filter(content_type__model='customer'))
+    [*Permission.objects.filter(content_type__model='customer'), *Permission.objects.filter(content_type__model='actions'), *Permission.objects.filter(content_type__model='credit')])
 User.objects.get(username="worker").groups.add(Group_L4)
