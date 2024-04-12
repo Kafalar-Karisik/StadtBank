e4277e26c6c71bdeb9e3ea975b6aff1ccfac6dcf
Kafalar-Karisik <orsmes6145@gmail.com>
2024-04-12 15:38:22 +0200

    Script Update
    Auto Password changed from Zulu time to Timestamp

--- bin/TOTP.py
+++ bin/TOTP.py
@@ -1,19 +1,32 @@
-import datetime
+import time
 
-from django.contrib.auth.models import Group, User
+try:
+    from django.contrib.auth.models import User
+except:
+    pass
+
 
 # curl -X POST http://127.0.0.1/newPassw
 
 
 def newWorkerPassword() -> str:
-    """Change Worker Password
-    Chenge the Worker user password with the Zulu Time
-    Example:
-        UTC: 10:21
-        Password: 1021Z
-    """
-    passw = datetime.datetime.now(datetime.UTC).strftime('%H%MZ')
+    """Change Worker password with the last 5 character of current timestamp (It is not ONE TIME Password!!!)"""
+    passw = str(int(time.time()))[-5:]
     user = User.objects.get(username="worker")
-    user.set_password(datetime.datetime.now(datetime.UTC).strftime('%H%MZ'))
+    user.set_password(passw)
     user.save()
     return passw
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    import django
+    sys.path.append(os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..')))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'StadtBank.settings')
+    django.setup()
+    from django.contrib.auth.models import User
+    print(newWorkerPassword())
