eaa846eda308e8f2498a5437680680d039fcdb55
Kafalar-Karisik <orsmes6145@gmail.com>
2024-04-24 21:09:59 +0200

    Documentation Update

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,8 @@ author = 'Kafalar Karışık'
 extensions = [
     'sphinxcontrib_django',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    "sphinxext.opengraph",
 ]
 
 templates_path = ['_templates']
@@ -48,8 +49,20 @@ html_title = "StadtBank"
 # The suffix of source filenames.
 source_suffix = '.rst'
 
+# -- Open Graph Configuration ------------------------------------------------
+# https://sphinxext-opengraph.readthedocs.io/en/latest/
 
-# Adjust the path as needed
+ogp_site_url = "https://kafalar-karisik.github.io/StadtBank/"
+ogp_image = "https://opengraph.githubassets.com/6271a1f0466d012fbbf634faea54b21639eff616dbdd86109debc296a45803d8/Kafalar-Karisik/StadtBank"
+ogp_description_length = 300
+ogp_type = "article"
+# ogp_custom_meta_tags = [
+#    '<meta property="og:ignore_canonical" content="true" />',
+# ]
+ogp_enable_meta_description = True
+
+
+# Django Setup
 sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'StadtBank.settings'
 django.setup()
