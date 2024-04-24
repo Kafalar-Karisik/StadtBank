7e55b287c5942a7db4778e7414f0850bd09f2ce6
Kafalar-Karisik <orsmes6145@gmail.com>
2024-04-24 21:11:24 +0200

    Documentation Update

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,8 @@ source_suffix = '.rst'
 
 ogp_site_url = "https://kafalar-karisik.github.io/StadtBank/"
 ogp_image = "https://opengraph.githubassets.com/6271a1f0466d012fbbf634faea54b21639eff616dbdd86109debc296a45803d8/Kafalar-Karisik/StadtBank"
-ogp_description_length = 300
-ogp_type = "article"
+# ogp_description_length = 300
+# ogp_type = "article"
 # ogp_custom_meta_tags = [
 #    '<meta property="og:ignore_canonical" content="true" />',
 # ]
