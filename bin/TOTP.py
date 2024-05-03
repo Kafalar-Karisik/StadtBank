ba89f94300de33a1168867a089b3531b0bb9ce24
Kafalar-Karisik <orsmes6145@gmail.com>
2024-05-03 17:01:16 +0200

    Update TOTP.py

--- bin/TOTP.py
+++ bin/TOTP.py
@@ -6,25 +6,25 @@ except:
     pass
 
 
-# curl -X POST http://127.0.0.1/newPass
+# curl -X POST -d "password=PASSWORD" 127.0.0.1/newPass
 
 class TOTPError(Exception):
     pass
 
 
 # You can customize it with your Super User Account
-def newWorkerPassword(adminPass: str):
+def newWorkerPassword(adminPass: str = ""):
     """Change Worker password. It will return the new Password"""
     if User.objects.filter(username='admin', is_superuser=True).exists():
         # If there is, prompt for the password
-        admin_password = input(
-            "Enter the password for the superuser 'admin': ")
+        if not adminPass:
+            adminPass = input("Enter the password for the superuser 'admin': ")
 
         # Retrieve the user with the username "admin"
         admin_user = User.objects.get(username='admin')
 
         # Check if the provided password is correct
-        if admin_user.check_password(admin_password):
+        if admin_user.check_password(adminPass):
             passw = str(int(time.time()))[-5:]
             user = User.objects.get(username="worker")
             user.set_password(passw)
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     sys.path.append(os.path.abspath(os.path.join(
         os.path.dirname(__file__), '..')))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'StadtBank.settings')
+                            'StadtBank.settings')
     django.setup()
     from django.contrib.auth.models import User
 
