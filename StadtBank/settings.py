c1f30eea925b5e79879546bbd7345b66cb86d605
Ömer Rasim <orsmes6145@gmail.com>
2024-10-08 23:29:01 +0200

    refactor: move django database to folder
    Set Django database location in database folder.

--- StadtBank/settings.py
+++ StadtBank/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'StadtBank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
     }
 }
 
